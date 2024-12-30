8112ac9db378c261c2e0efbc22bb1a372a188289
Chris Chua <chris.sirhc@gmail.com>
2024-12-30 19:39:31 +0800

    simplify further

--- 2024-12-30/main.ts
+++ 2024-12-30/main.ts
@@ -3,7 +3,7 @@ const DATE_FORMAT = new Intl.DateTimeFormat('en-US', {
 });
 export function newYearsDay(year: number): string {
   const date = Date.UTC(year, 0, 1);
-  return DATE_FORMAT.formatToParts(date)[0].value;
+  return DATE_FORMAT.format(date);
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
